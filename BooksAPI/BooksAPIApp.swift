605f43f090d8bd2a55fba63c99b3f8ece19abac4
Assem <assemsarsenbay08@gmail.com>
2021-05-13 13:37:20 +0600

    Initial Commit

---
+++ BooksAPI/BooksAPIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksAPIApp.swift
+//  BooksAPI
+//
+//  Created by Assem on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct BooksAPIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
